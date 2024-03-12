1cd9bfbe4ca0a426fe1119ec306c30a1afdf2e2b
Anurag Tiwari <aru.anurag@gmail.com>
2024-03-11 20:11:10 -0400

    Added handlers for pod and deployment

--- cmd/serverd/main.go
+++ cmd/serverd/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"log"
-
 	"github.com/aruanurag/preflight-inspector/http"
+	log "k8s.io/klog/v2"
 )
 
 func main() {
 	server := http.NewServer("8081")
 	err := server.ListenAndServe()
 	log.Fatal(err)
+	log.Infof("Starting server on port: %s", 8081)
+	if err := server.ListenAndServe(); err != nil {
+		log.Errorf("Failed to listen and serve: %v", err)
+	}
 }
